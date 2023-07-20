609f58bd38809c25b9586e55cbea756b54ecc727
Deven Anthony <deven.anthony@thelastmile.org>
2023-07-20 08:30:51 -0700

    adding config

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,8 @@
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {
+  theme = 'tokyodark',
+  transparency = true,
+}
+ M.plugins = "custom.plugins"
+ return M 
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,31 @@
+-- In order to modify the `lspconfig` configuration:
+local plugins = {
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function ()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {"williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "pyright",
+        "mypy",
+        "ruff",
+        "ansible-language-server",
+        "ansible-lint",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+}
+
+return plugins
